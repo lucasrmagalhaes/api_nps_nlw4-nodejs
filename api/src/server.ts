a298f2ae3d6759128a0146f4c4939cba1e2619fe
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-02-26 10:23:57 -0300

    entidade users pronta

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,29 +1,12 @@
 import 'reflect-metadata';
 import express from "express";
 import "./database";
+import { router } from './router';
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
-// http://localhost:3333/users/
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World" })
-    // return response.send("Hello World - NLW#04")
-})
-
-// 1 param => Rota(Recurso API)
-// 2 param => resquest, response
-app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({ message: "Os dados foram salvos com sucesso!" })
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => console.log("Server is running!"))
 
