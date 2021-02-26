f9945172619ab4b8fef12a89e526f7fa69e6daa2
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-02-26 00:02:12 -0300

    migrations rodando

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import 'reflect-metadata';
+import express from "express";
+import "./database";
 
 const app = express();
 
@@ -11,15 +13,14 @@ const app = express();
  */
 
 // http://localhost:3333/users/
-app.get("", (request, response) => {
+app.get("/", (request, response) => {
     return response.json({ message: "Hello World" })
     // return response.send("Hello World - NLW#04")
 })
 
 // 1 param => Rota(Recurso API)
 // 2 param => resquest, response
-
-app.post("/", (resquest, response) => {
+app.post("/", (request, response) => {
     // Recebeu os dados para salvar
     return response.json({ message: "Os dados foram salvos com sucesso!" })
 })
