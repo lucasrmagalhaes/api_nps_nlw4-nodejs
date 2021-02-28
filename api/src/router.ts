85843de95260912881ad9b3ac6821608b73a9522
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-02-28 10:02:22 -0300

    criado e testado o controller de resposta do usuário

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from "./controllers/UserController"
@@ -8,8 +9,10 @@ const router = Router();
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.get("/surveys", surveysController.show);
+router.get("/answers/:value", answerController.execute);
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
