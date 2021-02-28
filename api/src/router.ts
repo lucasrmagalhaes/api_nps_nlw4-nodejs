4653488c87d932be2170be22c9fa465c7cb577e7
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-02-28 10:26:45 -0300

    cálculo do NPS finalizado

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from "./controllers/UserController"
@@ -10,9 +11,11 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.get("/surveys", surveysController.show);
 router.get("/answers/:value", answerController.execute);
+router.get("/nps/:survey_id", npsController.execute);
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
